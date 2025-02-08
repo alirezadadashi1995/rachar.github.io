e2e400670376dd1448a50ae5d5f2298ce967226d
alirezadadashi1995 <alirezadadashi1995@gmail.com>
2025-02-08 15:16:16 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -27,3 +27,19 @@ setInterval(nextSlide, 3000);
 
 // نمایش اسلاید اول در ابتدا
 showSlide(currentSlide);
+let startX = 0;
+let endX = 0;
+
+document.querySelector(".slider").addEventListener("touchstart", (e) => {
+    startX = e.touches[0].clientX;
+});
+
+document.querySelector(".slider").addEventListener("touchend", (e) => {
+    endX = e.changedTouches[0].clientX;
+    if (startX > endX + 50) {
+        nextSlide(); // سوایپ به چپ (نمایش اسلاید بعدی)
+    } else if (startX < endX - 50) {
+        prevSlide(); // سوایپ به راست (نمایش اسلاید قبلی)
+    }
+});
+
