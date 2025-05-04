f756a2b0caa17511ea8f5d3a593657b6d145497d
alirezadadashi1995 <alirezadadashi1995@gmail.com>
2025-05-04 11:01:06 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,23 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll(".slide");
-const totalSlides = slides.length;
-const prevButton = document.getElementById("prevSlide");
-const nextButton = document.getElementById("nextSlide");
+<script>
+  let slides = document.querySelectorAll('.slide');
+  let currentSlide = 0;
 
-function showSlide(index) {
-    slides.forEach(slide => slide.classList.remove("active"));
-    slides[index].classList.add("active");
-}
+  function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[index].classList.add('active');
+  }
 
-function nextSlide() {
-    currentSlide = (currentSlide + 1) % totalSlides;
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
     showSlide(currentSlide);
-}
+  }
 
-function prevSlide() {
-    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
-    showSlide(currentSlide);
-}
+  // شروع نمایش اولیه
+  showSlide(currentSlide);
 
-nextButton.addEventListener("click", nextSlide);
-prevButton.addEventListener("click", prevSlide);
-
-// نمایش خودکار هر ۳ ثانیه
-setInterval(nextSlide, 3000);
-
-// نمایش اسلاید اول در ابتدا
-showSlide(currentSlide);
-let startX = 0;
-let endX = 0;
-
-document.querySelector(".slider").addEventListener("touchstart", (e) => {
-    startX = e.touches[0].clientX;
-});
-
-document.querySelector(".slider").addEventListener("touchend", (e) => {
-    endX = e.changedTouches[0].clientX;
-    if (startX > endX + 50) {
-        nextSlide(); // سوایپ به چپ (نمایش اسلاید بعدی)
-    } else if (startX < endX - 50) {
-        prevSlide(); // سوایپ به راست (نمایش اسلاید قبلی)
-    }
-});
+  // تعویض خودکار هر 5 ثانیه
+  setInterval(nextSlide, 5000);
+</script>
 
 // انتخاب عناصر مورد نیاز
 const hamburger = document.querySelector('.menu-icon');
